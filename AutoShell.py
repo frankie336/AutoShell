19fa08fd3e232d49d2c654563369f79c5229f3c1
frankie336 <34579453+frankie336@users.noreply.github.com>
2021-01-19 21:34:11 +0000

    update
    update

--- AutoShell.py
+++ AutoShell.py
@@ -1,137 +1,169 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan 16 11:39:36 2021
 
-@author: Thanos
+"""
+Created on Mon Jan 11 20:08:27 2021
+#! Python 3.8
+
+@author: Francis Neequaye
+         francis.neequaye@gmail.com
 """
 
-import socket
-import paramiko
-import select
-import re  
+
+import os 
 import logging
+import socketserver
+import re
 import time
+import socket
+import pandas as pd
 
+
+try:
+    import paramiko
+except ImportError:
+    print("Trying to Install required module: paramiko\n")
+    os.system('python -m pip install paramiko')
+import paramiko
+from paramiko import SSHClient, BadAuthenticationType
+
+
+try:
+    import select
+except ImportError:
+    print("Trying to Install required module: select\n")
+    os.system('python -m pip install select')
+import select
+
+
+
+
+
+"""
+An Interactive automated SSH Client:
+ - Will connect to target device when triggered and run 
+   specified commands
+"""
 PROMPT_PATTERN = r'\S+#'
 logging.basicConfig(level=logging.DEBUG)
 log = logging
-paramiko.util.log_to_file('show_me_the_vlan_log.log')
-
-class Channel:
-    def __init__(self):
-
-        with open("commands.txt") as file:
-             self.commands = [line.strip() for line in file]
-
-        self.cmditer = iter(self.commands)
+paramiko.util.log_to_file('Auto_Shell_log.log')
+class Ssh(object):
+    def __init__(self, host, commands, creds=None,
+                 prompt_pattern=PROMPT_PATTERN, init_commands=None):
+        if creds is None:
+            raise RuntimeError('You must supply username and password!!')
         
-       
-     
 
+        
+        
 
-        pat1 = [r'\S+>$',r'\S+#$',r'\S+:$']
-        self.prompt =  re.compile( '|'.join(pat1))
+        self.commands = commands
+
+        username, password = creds
+        self.creds = creds
+        self.username = username
+        self.password = password
+
+        self.prompt = re.compile(prompt_pattern)
+        if init_commands is None:
+            init_commands = []
+        self.init_commands = init_commands
+
+        self.results = []
+        self.host = host
+
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        self.ssh = ssh
+        self.initialized = False
+
+    def run(self):
+  
+        self.ssh.connect(self.host, username=self.username,
+                         password=self.password,look_for_keys=False)
 
       
-        self.results = []
-
-        
-
-   
-    def setup(self):
-     """
-     Settting up the connection:
-     """
-     
-     ssh = paramiko.SSHClient()
-     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-     self.ssh = ssh
-     self.initialized = False
-
-
-
-     self.port = 22
-     self.username = 'cisco'
-     self.password = 'cisco'
-     self.init_commands = ['terminal length 0\n','sh clock\n','conf t\n','int GigabitEthernet1/0\n','shut\n']
-
-     
-    
-
-    def connect(self):
-    
-        self.ssh.connect(self.ip,self.port,self.username,self.password,look_for_keys=False
-                  )
         shell = self.ssh.invoke_shell()
         shell.settimeout(0.0)
         self.shell = shell
-        
-        self.resp = ''
-        print("Device")
-        self.interact()
 
+      
+        self.cmditer = iter(self.commands)
 
+      
+        self.data = ''
 
-
+ 
+        results = self.interact()
+        return results
 
     def interact(self):
         """Interact with the device using the SSH shell."""
         shell = self.shell
-        while True:
-      
-            r, w, e = select.select([shell], [], [])
-        
-            if shell in r:
-                try:
-               
-                    bytes = shell.recv(16000)
 
+   
+        while True:
+         
+            r, w, e = select.select([shell], [], [])
+         
+            if shell in r:
+            
+                try:
+                 
+                    bytes = shell.recv(1024)
+
+                  
                     if len(bytes) == 0:
                         break
                     
-             
+                   
                     self.data_received(bytes)
 
+               
                 except (socket.timeout, socket.error) as err:
                     log.error(str(err))
 
+          
             else:
                 break
 
+       
         shell.close()
+
+       
         return self.results
 
-
     def data_received(self, bytes):
-        
+     
         log.debug('Got bytes: %r' % bytes)
-        self.resp += bytes.decode(encoding='UTF-8')
-        log.debug(' Buffered: %r' % self.resp)
+        self.data += bytes.decode(encoding='UTF-8')
+        log.debug(' Buffered: %r' % self.data)
 
 
-        m = self.prompt.search(self.resp)
+        m = self.prompt.search(self.data)
         if not m:
             return None
-
         log.debug('STATE: prompt %r' % m.group())
 
+        
+        result = self.data
+        #result = self.data[:m.start()]
+        #result = result[result.find('\n')+1:]
 
-        result = self.resp[:m.start()]
-        result = result[result.find('\n')+1:]
-
+      
         if self.initialized:
             self.results.append(result)
-            
         
+        """
+        """
         self.mode  =  m.group()# A capture group for the current mode of the cli
         
+       
+
+
         self.send_next()
 
-
     def send_next(self):
-
-        self.resp = ''
-        
 
         """
         If the device is sitting at user exec mode 
@@ -139,95 +171,177 @@ class Channel:
         """
         exec_promt = '>'
         is_in_user_exec = True if re.search(exec_promt, self.mode, re.MULTILINE) else False
-     
+
+
+        """
+        To handle cisco enable mode uncomment this block of code
+        """
+        """
+        if not self.initialized:
+          if is_in_user_exec == True:
+              while True:
+                  mode_select = input('Cisco User exec mode sensed. Press "y" to enter enable mode or "n" to continue in user exec mode: ')
+                  if mode_select =='y':
+                      self.shell.send('enable\n')
+                      time.sleep(.5)
+                      self.shell.send('cisco\n')
+                      break
+
+                  if mode_select=='n':
+                      break
+          else:
+              pass
+        """ 
+
+        
+    
+        self.data = ''
 
         if not self.initialized:
-            if is_in_user_exec == True:
-                while True:
-                    mode_select = input('Cisco User exec mode sensed. Press "y" to enter enable mode or "n" to continue in user exec mode: ')
-                    if mode_select =='y':
-                        self.shell.send('enable\n')
-                        time.sleep(.5)
-                        self.shell.send('cisco\n')
-                        break
-
-                    if mode_select=='n':
-                        break
+            if self.init_commands:
+                next_init = self.init_commands.pop(0)
+                self.shell.send(next_init)
+                return None
             else:
-                pass 
+                log.debug('Successfully initialized for command execution')
+                self.initialized = True
 
        
-
-          
-        print("******************starting main commands************************")
-
         try:
-            time.sleep(1)
             next_command = self.cmditer.__next__() # Get the next command
         except StopIteration:
-            self.close() # Or disconnect
-            #return None
-            pass
+            self.close() 
+            return None
 
-
-        # Try to send the next command
+      
         if next_command is None:
-            self.results[0].append(None) # Store a null command w/ null result
+            self.results.append(None) # Store a null command w/ null result
             self.send_next() # Fetch the next command
         else:
             log.debug('sending %r' % next_command)
-            self.shell.send(next_command + '\n')  # Send this command 
-
-        
-        
-
-
-
-    
-    def CommandOutput(self):
-
-        
-        #print('++++++++++++++++++++++',self.output)
-        #print(len(self.output))#
-
-        print("++++++++++RESULTS+++++++++",self.results)
-
-      
-
-       
-        
-       
+            self.shell.send(next_command + '\n')  # Send this command
 
     def close(self):
         """Close the SSH connection."""
         self.ssh.close()
 
-    def main(self): 
+
+class LoopDevices:
+    def __init__(self):
         
-        self.output = []
-    
+        """
+        - Open the hosts.txt file and read each line into a list
+        -  Make the list an object in this constructor function
+        - Make the length of the self.hosts list an object 
+        """
         with open("hosts.txt") as file:
-         hosts = [line.strip() for line in file]
+             self.hosts = [line.strip() for line in file]
+
+        self.host_len = len(self.hosts)
+        
+        """
+        The Number of nested lists made is now tied to
+        the length of the self.hosts list  
+        """
+        self.output = [[] for _ in range(self.host_len)]
+
+        
+        """
+        - Open the commands.txt file and read each line into a list
+        -  Make the list an object in this constructor function
+        """
+        with open("commands.txt") as file:
+             self.commands = [line.strip() for line in file]
+
+
+    def ProcessOutput(self):
+         
+        if len(self.failed_hosts)>0:
+
+        
+
+
+
+        """
+        Searching for the hostname of the device:
+        """
+        host_pattern = r'(.*>)|(.*#)'
+        self.find_host = re.compile(host_pattern)
+        
+        
+        self.host_names = []
+
+        for i in range(self.host_len):
+             e = self.find_host.search(self.read_out[i][i][0])
+             self.host_names.append(e.group())
+
+        
+        remove = removetable = str.maketrans('', '', '>')
+        self.host_names = [s.translate(remove) for s in self.host_names]
+        
+             
+        
+        
+        for i in range(self.host_len):
+            self.diag=self.read_out[0][i]
+            with open(self.host_names[i]+'_output.txt', 'w') as f:
+                for item in self.diag:
+                    f.write("%s\n" % item)
+
+
+
+
+        
+    def main(self):
+
+        username = 'cisci'#NEEDS TO BE SET TO THE APPROPRIATE DEVICE MANAGEMENT USERNAME
+        password = 'cisci'#NEEDS TO BE SET TO THE APPROPRIATE DEVICE MANAGEMENT PASSWORD
+        creds = (username, password)
+
+
+        pat1 = [r'\S+>$',r'\S+#$']
+        prompt_pattern =  re.compile( '|'.join(pat1))
+        init_commands = ['set cli scripting-mode on\n', 'terminal length 0\n']
+        commands = self.commands
+        host = self.hosts
+        
+        """
+        1. Loop the following:
+         - SSH Connect connect to each device in the hosts.txt file
+         - Append the output of commands and diagnostics to the next nested 
+         list in self.output, using the [i]index in the range loop
+        """
+
+
+        self.failed_hosts = []
+
+        for i in range(self.host_len):
+         for x in host:
+            try:
+                c = Ssh(x, commands, creds, prompt_pattern, init_commands)
+                results = c.run()
+                self.output[i].append(results)
+            except:
+                self.failed_hosts.append(x)
+                self.failed_hosts = pd.unique(self.failed_hosts).tolist()
+
+
+
+       
+        self.read_out = self.output
+
 
         
         
-        for x in hosts:
-            self.ip = x
-            p1 = Channel
-            p1.setup(self)
-            p1.connect(self)
-            
-            #p1.send_next(self)
-            #self.output.append(self.results)
-            
-        p1.CommandOutput(self)
-    
 
 if __name__ == "__main__":
+    p1 = LoopDevices()
+    p1.main()
+    p1.ProcessOutput()
     
-     run = Channel()
-     run.main()
     
-
-
-
+     
+     
+ 
+    
+     
